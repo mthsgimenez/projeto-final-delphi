e94e00b70c5e05e4ce2e923b6568f0178ec9e459
unknown <82736724+mthsgimenez@users.noreply.github.com>
2025-09-02 08:35:44 -0300

    :sparkles: feat: Visual da view de login completo #2

--- src/views/LoginView.pas
+++ src/views/LoginView.pas
@@ -5,21 +5,29 @@ interface
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.StdCtrls,
-  Vcl.Imaging.jpeg, Vcl.Buttons;
+  Vcl.Imaging.jpeg, Vcl.Buttons, Vcl.Imaging.pngimage;
 
 type
   TformLogin = class(TForm)
     panelBackground: TPanel;
     imageBackground: TImage;
     gridPanelLayout: TGridPanel;
-    panelInputs: TPanel;
-    editUser: TEdit;
-    panelButton: TPanel;
-    buttonLogin: TSpeedButton;
-    panelEditUser: TPanel;
-    panelEditPassword: TPanel;
-    editPassword: TEdit;
+    panelLoginForm: TPanel;
+    shapeLoginForm: TShape;
+    gridPanelLoginForm: TGridPanel;
     labelTitle: TLabel;
+    panelEdits: TPanel;
+    panelEditUser: TPanel;
+    shapeEditUser: TShape;
+    imageUserIcon: TImage;
+    editUser: TEdit;
+    panelEditPassword: TPanel;
+    shapeEditPassword: TShape;
+    imageLockIcon: TImage;
+    editPassword: TEdit;
+    panelButtonLogin: TPanel;
+    shapeButtonLogin: TShape;
+    buttonLogin: TSpeedButton;
     procedure buttonLoginClick(Sender: TObject);
   private
     { Private declarations }
