830651624c98fa58e8b523863114c9beafa73264
mthsgimenez <82736724+mthsgimenez@users.noreply.github.com>
2025-09-15 16:21:01 -0300

    :sparkles: feat (DBConfigDTO): Adicionado método para validar DTO

--- src/DTO/DBConfigDTO.pas
+++ src/DTO/DBConfigDTO.pas
@@ -2,14 +2,28 @@ unit DBConfigDTO;
 
 interface
 
+  uses System.SysUtils, System.Classes;
+
   type TDBConfigDTO = record
     server: String;
     port: Integer;
     database: String;
     user: String;
     password: String;
+    function ValidateDTO: TStringList;
   end;
 
 implementation
+
+function TDBConfigDTO.ValidateDTO: TStringList;
+var
+  errorFields: TStringList;
+begin
+  errorFields := TStringList.Create;
+  if Trim(Self.server) = '' then errorFields.Add('campo "server" não pode estar vazio.');
+  if Trim(Self.database) = '' then errorFields.Add('campo "database" não pode estar vazio.');
+  if Trim(Self.user) = '' then errorFields.Add('campo "user" não pode estar vazio.');
+  Result := errorFields;
+end;
 
 end.
