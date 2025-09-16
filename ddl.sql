822fee0531a168a0f4c0584a09efb28119238a2c
mthsgimenez <82736724+mthsgimenez@users.noreply.github.com>
2025-09-16 16:21:33 -0300

    :sparkles: feat: Adicionada coluna de login e hash na tabela users

--- ddl.sql
+++ ddl.sql
@@ -1,20 +1,106 @@
 -- DROP SCHEMA public;
 
 CREATE SCHEMA public AUTHORIZATION pg_database_owner;
--- public.users definição
 
--- Drop table
+-- DROP SEQUENCE public.permissions_id_seq;
 
--- DROP TABLE public.users;
+CREATE SEQUENCE public.permissions_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 2147483647
+	START 1
+	CACHE 1
+	NO CYCLE;
+-- DROP SEQUENCE public.permissions_users_id_seq;
 
-CREATE TABLE public.users (
-	id serial4 NOT NULL,
-	"name" varchar(50) NOT NULL,
-	CONSTRAINT users_pk PRIMARY KEY (id)
-);
+CREATE SEQUENCE public.permissions_users_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 2147483647
+	START 1
+	CACHE 1
+	NO CYCLE;
+-- DROP SEQUENCE public.purchase_order_tools_id_seq;
 
+CREATE SEQUENCE public.purchase_order_tools_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 2147483647
+	START 1
+	CACHE 1
+	NO CYCLE;
+-- DROP SEQUENCE public.purchase_orders_id_seq;
 
--- public.permissions definição
+CREATE SEQUENCE public.purchase_orders_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 2147483647
+	START 1
+	CACHE 1
+	NO CYCLE;
+-- DROP SEQUENCE public.service_order_tools_id_seq;
+
+CREATE SEQUENCE public.service_order_tools_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 2147483647
+	START 1
+	CACHE 1
+	NO CYCLE;
+-- DROP SEQUENCE public.service_orders_id_seq;
+
+CREATE SEQUENCE public.service_orders_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 2147483647
+	START 1
+	CACHE 1
+	NO CYCLE;
+-- DROP SEQUENCE public.storages_id_seq;
+
+CREATE SEQUENCE public.storages_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 2147483647
+	START 1
+	CACHE 1
+	NO CYCLE;
+-- DROP SEQUENCE public.suppliers_id_seq;
+
+CREATE SEQUENCE public.suppliers_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 2147483647
+	START 1
+	CACHE 1
+	NO CYCLE;
+-- DROP SEQUENCE public.tools_id_seq;
+
+CREATE SEQUENCE public.tools_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 2147483647
+	START 1
+	CACHE 1
+	NO CYCLE;
+-- DROP SEQUENCE public.tools_models_id_seq;
+
+CREATE SEQUENCE public.tools_models_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 2147483647
+	START 1
+	CACHE 1
+	NO CYCLE;
+-- DROP SEQUENCE public.users_id_seq;
+
+CREATE SEQUENCE public.users_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 2147483647
+	START 1
+	CACHE 1
+	NO CYCLE;-- public.permissions definição
 
 -- Drop table
 
@@ -58,6 +144,22 @@ CREATE TABLE public.suppliers (
 );
 
 
+-- public.users definição
+
+-- Drop table
+
+-- DROP TABLE public.users;
+
+CREATE TABLE public.users (
+	id serial4 NOT NULL,
+	"name" varchar(50) NOT NULL,
+	login varchar(25) NOT NULL,
+	hash varchar(60) NOT NULL,
+	CONSTRAINT users_pk PRIMARY KEY (id),
+	CONSTRAINT users_unique UNIQUE (login)
+);
+
+
 -- public.permissions_users definição
 
 -- Drop table
@@ -71,6 +173,36 @@ CREATE TABLE public.permissions_users (
 	CONSTRAINT permissions_users_pk PRIMARY KEY (id),
 	CONSTRAINT permissions_users_permissions_fk FOREIGN KEY (id_permission) REFERENCES public.permissions(id) ON DELETE CASCADE ON UPDATE CASCADE,
 	CONSTRAINT permissions_users_users_fk FOREIGN KEY (id_user) REFERENCES public.users(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
+-- public.purchase_orders definição
+
+-- Drop table
+
+-- DROP TABLE public.purchase_orders;
+
+CREATE TABLE public.purchase_orders (
+	id serial4 NOT NULL,
+	id_supplier int4 NOT NULL,
+	status varchar(15) DEFAULT 'open'::character varying NOT NULL,
+	CONSTRAINT purchase_orders_pk PRIMARY KEY (id),
+	CONSTRAINT purchase_orders_suppliers_fk FOREIGN KEY (id_supplier) REFERENCES public.suppliers(id) ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+
+-- public.service_orders definição
+
+-- Drop table
+
+-- DROP TABLE public.service_orders;
+
+CREATE TABLE public.service_orders (
+	id serial4 NOT NULL,
+	id_supplier int4 NOT NULL,
+	status varchar(15) DEFAULT 'open'::character varying NOT NULL,
+	CONSTRAINT service_orders_pk PRIMARY KEY (id),
+	CONSTRAINT service_orders_suppliers_fk FOREIGN KEY (id_supplier) REFERENCES public.suppliers(id) ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 
@@ -95,40 +227,6 @@ CREATE TABLE public.tools_models (
 );
 
 
--- public.tools definição
-
--- Drop table
-
--- DROP TABLE public.tools;
-
-CREATE TABLE public.tools (
-	id serial4 NOT NULL,
-	id_tool_model int4 NOT NULL,
-	state varchar(15) DEFAULT 'new'::character varying NOT NULL,
-	batch varchar(30) NOT NULL,
-	honing_num int4 DEFAULT 0 NOT NULL,
-	id_storage int4 NOT NULL,
-	CONSTRAINT tools_pk PRIMARY KEY (id),
-	CONSTRAINT tools_storages_fk FOREIGN KEY (id_storage) REFERENCES public.storages(id) ON DELETE RESTRICT ON UPDATE CASCADE,
-	CONSTRAINT tools_tools_models_fk FOREIGN KEY (id_tool_model) REFERENCES public.tools_models(id) ON DELETE RESTRICT ON UPDATE CASCADE
-);
-
-
--- public.purchase_orders definição
-
--- Drop table
-
--- DROP TABLE public.purchase_orders;
-
-CREATE TABLE public.purchase_orders (
-	id serial4 NOT NULL,
-	id_supplier int4 NOT NULL,
-	status varchar(15) DEFAULT 'open'::character varying NOT NULL,
-	CONSTRAINT purchase_orders_pk PRIMARY KEY (id),
-	CONSTRAINT purchase_orders_suppliers_fk FOREIGN KEY (id_supplier) REFERENCES public.suppliers(id) ON DELETE RESTRICT ON UPDATE CASCADE
-);
-
-
 -- public.purchase_order_tools definição
 
 -- Drop table
@@ -146,18 +244,22 @@ CREATE TABLE public.purchase_order_tools (
 );
 
 
--- public.service_orders definição
+-- public.tools definição
 
 -- Drop table
 
--- DROP TABLE public.service_orders;
+-- DROP TABLE public.tools;
 
-CREATE TABLE public.service_orders (
+CREATE TABLE public.tools (
 	id serial4 NOT NULL,
-	id_supplier int4 NOT NULL,
-	status varchar(15) DEFAULT 'open'::character varying NOT NULL,
-	CONSTRAINT service_orders_pk PRIMARY KEY (id),
-	CONSTRAINT service_orders_suppliers_fk FOREIGN KEY (id_supplier) REFERENCES public.suppliers(id) ON DELETE RESTRICT ON UPDATE CASCADE
+	id_tool_model int4 NOT NULL,
+	state varchar(15) DEFAULT 'new'::character varying NOT NULL,
+	batch varchar(30) NOT NULL,
+	honing_num int4 DEFAULT 0 NOT NULL,
+	id_storage int4 NOT NULL,
+	CONSTRAINT tools_pk PRIMARY KEY (id),
+	CONSTRAINT tools_storages_fk FOREIGN KEY (id_storage) REFERENCES public.storages(id) ON DELETE RESTRICT ON UPDATE CASCADE,
+	CONSTRAINT tools_tools_models_fk FOREIGN KEY (id_tool_model) REFERENCES public.tools_models(id) ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 
