d09b61dc45415abc4c5f52b33bad514b5f4cffa7
mthsgimenez <82736724+mthsgimenez@users.noreply.github.com>
2025-09-17 17:00:58 -0300

    :sparkles: feat: Criada classe base para repositories que utilizam o banco

---
+++ src/model/repository/util/RepositoryBase.pas
@@ -0,0 +1,34 @@
+unit RepositoryBase;
+
+interface
+
+uses DBConnection, FireDAC.Comp.Client, FireDAC.DApt;
+
+type TRepositoryBase = class
+  protected
+    Query: TFDQuery;
+    Connection: TFDConnection;
+  public
+    constructor Create;
+    destructor Destroy; override;
+end;
+
+implementation
+
+{ TRepositoryBase }
+
+constructor TRepositoryBase.Create;
+begin
+  inherited Create;
+  Self.Connection := DBConnection.Connection.FDConnection;
+  Self.Query := TFDQuery.Create(Self.Connection);
+  Self.Query.Connection := Self.Connection;
+end;
+
+destructor TRepositoryBase.Destroy;
+begin
+  Self.Query.Free;
+  inherited Destroy;
+end;
+
+end.
