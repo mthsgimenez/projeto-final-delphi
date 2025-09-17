73a935609c0e43ff0e18b90aa7272bf193997953
mthsgimenez <82736724+mthsgimenez@users.noreply.github.com>
2025-09-17 17:03:36 -0300

    :sparkles: feat: Criada interface com metodos crud #11

---
+++ src/model/repository/interface/CrudRepositoryInterface.pas
@@ -0,0 +1,18 @@
+unit CrudRepositoryInterface;
+
+interface
+
+uses System.Generics.Collections;
+
+type ICrudRepository<TEntity: class> = interface
+  function Save(aEntity: TEntity): TEntity;
+  function FindById(aId: Integer): TEntity;
+  function FindAll(): TObjectList<TEntity>;
+  function DeleteById(aId: Integer): Boolean;
+  function Delete(aEntity: TEntity): Boolean;
+  function ExistsById(aId: Integer): Boolean;
+end;
+
+implementation
+
+end.
