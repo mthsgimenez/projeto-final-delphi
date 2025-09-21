59bbefebc2831bcb155e13bed0f1a85698d90951
unknown <82736724+mthsgimenez@users.noreply.github.com>
2025-09-21 20:04:59 -0300

    :bug: fix (DBHelper): Utilizando function overload ao invés de Variants

--- src/model/repository/util/DBHelper.pas
+++ src/model/repository/util/DBHelper.pas
@@ -2,26 +2,32 @@ unit DBHelper;
 
 interface
 
-uses RepositoryBase, System.SysUtils, Variants;
+uses RepositoryBase, System.SysUtils;
 
 type TDBHelper = class(TRepositoryBase)
+  private
+
   public
-    function CheckIfAlreadyExists(aTable: String; aColumn: String; value: Variant): Boolean;
+    function CheckIfAlreadyExists(aTable: String; aColumn: String; value: Integer): Boolean; overload;
+    function CheckIfAlreadyExists(aTable: String; aColumn: String; value: String): Boolean; overload;
 end;
 
 implementation
 
 { TDBHelper }
 
-function TDBHelper.CheckIfAlreadyExists(aTable, aColumn: String;
-  value: Variant): Boolean;
+function TDBHelper.CheckIfAlreadyExists(aTable, aColumn,
+  value: String): Boolean;
 begin
-  if VarIsStr(value) then begin
-    Self.Query.SQL.Text := Format('SELECT COUNT(*) FROM %s WHERE %s=%s', [aTable, aColumn, QuotedStr(value)]);
-  end else begin
-    Self.Query.SQL.Text := Format('SELECT COUNT(*) FROM %s WHERE %s=%d', [aTable, aColumn, value]);
-  end;
+  Self.Query.SQL.Text := Format('SELECT COUNT(*) FROM %s WHERE %s=%s', [aTable, aColumn, QuotedStr(value)]);
+  Self.Query.Open();
+  Result := Self.Query.Fields[0].AsInteger > 0;
+end;
 
+function TDBHelper.CheckIfAlreadyExists(aTable, aColumn: String;
+  value: Integer): Boolean;
+begin
+  Self.Query.SQL.Text := Format('SELECT COUNT(*) FROM %s WHERE %s=%d', [aTable, aColumn, value]);
   Self.Query.Open();
   Result := Self.Query.Fields[0].AsInteger > 0;
 end;
