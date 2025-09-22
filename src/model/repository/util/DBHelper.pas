cab3f99c95b4976044f13985b8a99ca64a7559ee
mthsgimenez <82736724+mthsgimenez@users.noreply.github.com>
2025-09-22 15:23:01 -0300

    :sparkles: feat (DBHelper): Função para verificar se um valor existe ignorando a linha com id específicado

--- src/model/repository/util/DBHelper.pas
+++ src/model/repository/util/DBHelper.pas
@@ -8,8 +8,10 @@ type TDBHelper = class(TRepositoryBase)
   private
 
   public
-    function CheckIfAlreadyExists(aTable: String; aColumn: String; value: Integer): Boolean; overload;
-    function CheckIfAlreadyExists(aTable: String; aColumn: String; value: String): Boolean; overload;
+    function CheckIfAlreadyExists(aTable: String; aColumn: String; aValue: Integer): Boolean; overload;
+    function CheckIfAlreadyExists(aTable: String; aColumn: String; aValue: String): Boolean; overload;
+    function CheckIfAlreadyExistsExcludingId(aTable: String; aColumn: String; aValue: String; aId: Integer): Boolean; overload;
+    function CheckIfAlreadyExistsExcludingId(aTable: String; aColumn: String; aValue: Integer; aId: Integer): Boolean; overload;
 end;
 
 implementation
@@ -17,17 +19,37 @@ implementation
 { TDBHelper }
 
 function TDBHelper.CheckIfAlreadyExists(aTable, aColumn,
-  value: String): Boolean;
+  aValue: String): Boolean;
 begin
-  Self.Query.SQL.Text := Format('SELECT COUNT(*) FROM %s WHERE %s=%s', [aTable, aColumn, QuotedStr(value)]);
+  Self.Query.SQL.Text := Format('SELECT COUNT(*) FROM %s WHERE %s=%s',
+                          [aTable, aColumn, QuotedStr(aValue)]);
   Self.Query.Open();
   Result := Self.Query.Fields[0].AsInteger > 0;
 end;
 
 function TDBHelper.CheckIfAlreadyExists(aTable, aColumn: String;
-  value: Integer): Boolean;
+  aValue: Integer): Boolean;
 begin
-  Self.Query.SQL.Text := Format('SELECT COUNT(*) FROM %s WHERE %s=%d', [aTable, aColumn, value]);
+  Self.Query.SQL.Text := Format('SELECT COUNT(*) FROM %s WHERE %s=%d',
+                          [aTable, aColumn, aValue]);
+  Self.Query.Open();
+  Result := Self.Query.Fields[0].AsInteger > 0;
+end;
+
+function TDBHelper.CheckIfAlreadyExistsExcludingId(aTable, aColumn: String;
+  aValue, aId: Integer): Boolean;
+begin
+  Self.Query.SQL.Text := Format('SELECT COUNT(*) FROM %s WHERE %s=%d AND id != %d',
+                          [aTable, aColumn, aValue, aId]);
+  Self.Query.Open();
+  Result := Self.Query.Fields[0].AsInteger > 0;
+end;
+
+function TDBHelper.CheckIfAlreadyExistsExcludingId(aTable, aColumn,
+  aValue: String; aId: Integer): Boolean;
+begin
+  Self.Query.SQL.Text := Format('SELECT COUNT(*) FROM %s WHERE %s=%s AND id != %d',
+                          [aTable, aColumn, QuotedStr(aValue), aId]);
   Self.Query.Open();
   Result := Self.Query.Fields[0].AsInteger > 0;
 end;
