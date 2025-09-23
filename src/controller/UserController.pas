a41c6e2ca75a144d1f5ab5eeed69e7043bbb3505
mthsgimenez <82736724+mthsgimenez@users.noreply.github.com>
2025-09-23 17:15:41 -0300

    :sparkles: feat (UserController): Implementado GetUsers e EditUser

--- src/controller/UserController.pas
+++ src/controller/UserController.pas
@@ -2,7 +2,7 @@ unit UserController;
 
 interface
 
-uses UserRepository, UserModel, UserDTO;
+uses UserRepository, UserModel, UserDTO, System.Generics.Collections;
 
   type TUserController = class
     private
@@ -11,6 +11,8 @@ uses UserRepository, UserModel, UserDTO;
       constructor Create;
       destructor Destroy; override;
       function Login(aUser: TUserDTO): TUserModel;
+      function EditUser(aId: Integer; aData: TUserDTO): TUserModel;
+      function GetUsers: TObjectList<TUserModel>;
   end;
 
 implementation
@@ -27,6 +29,25 @@ destructor TUserController.Destroy;
 begin
   Self.repository.Free;
   inherited Destroy;
+end;
+
+function TUserController.EditUser(aId: Integer; aData: TUserDTO): TUserModel;
+var
+  user: TUserModel;
+begin
+  user := Self.repository.FindById(aId);
+  if not (user = nil) then begin
+    user.name := aData.name;
+    user.login := aData.login;
+    user.SetPassword(aData.password);
+    Self.repository.Save(user);
+  end;
+  Result := user;
+end;
+
+function TUserController.GetUsers: TObjectList<TUserModel>;
+begin
+  Result := Self.repository.FindAll;
 end;
 
 function TUserController.Login(aUser: TUserDTO): TUserModel;
