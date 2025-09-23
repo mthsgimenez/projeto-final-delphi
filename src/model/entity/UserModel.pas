8611a530521c201077bfc3752c8322141d3d8d5a
mthsgimenez <82736724+mthsgimenez@users.noreply.github.com>
2025-09-23 14:45:17 -0300

    :sparkles: feat (UserModel): Adicionado atributo hash e métodos relacionados
    Adicionado Getter e Setter, SetPassword para transformar a senha em hash e CheckPassword para comparar uma string com o hash

--- src/model/entity/UserModel.pas
+++ src/model/entity/UserModel.pas
@@ -2,13 +2,45 @@ unit UserModel;
 
 interface
 
+uses Bcrypt;
+
 type TUserModel = class
+  private
+    hash: String;
   public
     id: Integer;
     name: String;
     login: String;
+    procedure SetPassword(aPassword: String);
+    function CheckPassword(aPassword: String): Boolean;
+    procedure SetHash(aHash: String);
+    function GetHash: String;
 end;
 
 implementation
+
+{ TUserModel }
+
+function TUserModel.CheckPassword(aPassword: String): Boolean;
+var
+  needsRehash: Boolean;
+begin
+  Result := TBCrypt.CheckPassword(aPassword, Self.hash, needsRehash);
+end;
+
+function TUserModel.GetHash: String;
+begin
+  Result := Self.hash;
+end;
+
+procedure TUserModel.SetHash(aHash: String);
+begin
+  if Length(aHash) = 60 then Self.hash := aHash;
+end;
+
+procedure TUserModel.SetPassword(aPassword: String);
+begin
+  Self.hash := TBCrypt.HashPassword(aPassword, 12);
+end;
 
 end.
