e9aaae60c6bd7e6e8c9ac78b2ceeb996878ea2b7
mthsgimenez <82736724+mthsgimenez@users.noreply.github.com>
2025-09-29 17:18:19 -0300

    :sparkles: feat (UserDTO): Adicionado função de validação

--- src/DTO/UserDTO.pas
+++ src/DTO/UserDTO.pas
@@ -2,12 +2,28 @@ unit UserDTO;
 
 interface
 
+  uses System.SysUtils, System.Classes;
+
   type TUserDTO = record
     name: String;
     login: String;
     password: String;
+    function ValidateDTO(ensurePassword: Boolean): TStringList;
   end;
 
 implementation
+
+function TUserDTO.ValidateDTO(ensurePassword: Boolean): TStringList;
+var
+  errorFields: TStringList;
+begin
+  errorFields := TStringList.Create;
+  if Trim(Self.name) = '' then errorFields.Add('campo "nome" não pode estar vazio.');
+  if Trim(Self.login) = '' then errorFields.Add('campo "login" não pode estar vazio.');
+  if ensurePassword then
+    if Trim(Self.password) = '' then errorFields.Add('campo "senha" não pode estar vazio.');
+
+  Result := errorFields;
+end;
 
 end.
