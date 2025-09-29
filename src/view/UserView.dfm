6a85cd05276b3b0e9fd2ff12525cef7bc0a1ec4e
mthsgimenez <82736724+mthsgimenez@users.noreply.github.com>
2025-09-29 17:27:07 -0300

    :sparkles: feat (UserView): Adicionado cadastro, edição e exclusão de usuários #10

--- src/view/UserView.dfm
+++ src/view/UserView.dfm
@@ -26,12 +26,14 @@ object formUser: TformUser
     TabOrder = 0
     object tabList: TTabSheet
       Caption = 'tabList'
+      TabVisible = False
       OnShow = tabListShow
       object gridUsers: TStringGrid
         Left = 48
         Top = 48
         Width = 665
         Height = 345
+        ColCount = 3
         TabOrder = 0
       end
       object buttonCreate: TButton
@@ -50,11 +52,65 @@ object formUser: TformUser
         Height = 49
         Caption = 'Remover usu'#225'rio'
         TabOrder = 2
+        OnClick = buttonDeleteClick
+      end
+      object buttonEdit: TButton
+        Left = 296
+        Top = 442
+        Width = 153
+        Height = 49
+        Caption = 'Editar usu'#225'rio'
+        TabOrder = 3
+        OnClick = buttonEditClick
       end
     end
     object tabCreate: TTabSheet
       Caption = 'tabCreate'
       ImageIndex = 1
+      TabVisible = False
+      object editName: TEdit
+        Left = 296
+        Top = 152
+        Width = 145
+        Height = 23
+        TabOrder = 0
+        TextHint = 'Name'
+      end
+      object editLogin: TEdit
+        Left = 296
+        Top = 208
+        Width = 145
+        Height = 23
+        TabOrder = 1
+        TextHint = 'Login'
+      end
+      object editPassword: TEdit
+        Left = 296
+        Top = 272
+        Width = 145
+        Height = 23
+        PasswordChar = '*'
+        TabOrder = 2
+        TextHint = '********'
+      end
+      object buttonSave: TButton
+        Left = 384
+        Top = 336
+        Width = 145
+        Height = 33
+        Caption = 'Salvar'
+        TabOrder = 3
+        OnClick = buttonSaveClick
+      end
+      object buttonCancel: TButton
+        Left = 208
+        Top = 336
+        Width = 145
+        Height = 33
+        Caption = 'Cancelar'
+        TabOrder = 4
+        OnClick = buttonCancelClick
+      end
     end
   end
 end
--- src/view/UserView.pas
+++ src/view/UserView.pas
@@ -14,11 +14,23 @@ type
     gridUsers: TStringGrid;
     buttonCreate: TButton;
     buttonDelete: TButton;
+    buttonEdit: TButton;
+    editName: TEdit;
+    editLogin: TEdit;
+    editPassword: TEdit;
+    buttonSave: TButton;
+    buttonCancel: TButton;
     procedure buttonCreateClick(Sender: TObject);
     procedure tabListShow(Sender: TObject);
+    procedure buttonEditClick(Sender: TObject);
+    procedure buttonSaveClick(Sender: TObject);
+    procedure buttonDeleteClick(Sender: TObject);
+    procedure buttonCancelClick(Sender: TObject);
   private
     controller: TUserController;
+    selectedUserId: Integer;
     procedure UpdateGrid(aUsers: TObjectList<TUserModel>);
+    procedure ClearEdits;
   public
     constructor Create(AOwner: TComponent); override;
     destructor Destroy; override;
@@ -31,9 +43,90 @@ implementation
 
 {$R *.dfm}
 
+procedure TformUser.buttonCancelClick(Sender: TObject);
+begin
+  Self.pcontrolUser.ActivePage := Self.pcontrolUser.Pages[0];
+  Self.ClearEdits;
+  Self.selectedUserId := 0;
+end;
+
 procedure TformUser.buttonCreateClick(Sender: TObject);
 begin
   Self.pcontrolUser.ActivePage := Self.pcontrolUser.Pages[1];
+end;
+
+procedure TformUser.buttonDeleteClick(Sender: TObject);
+begin
+  Self.selectedUserId := StrToInt(Self.gridUsers.Cells[0, Self.gridUsers.Row]);
+
+  Self.controller.DeleteUser(Self.selectedUserId);
+
+  Self.selectedUserId := 0;
+
+  Self.tabListShow(nil);
+end;
+
+procedure TformUser.buttonEditClick(Sender: TObject);
+var
+  user: TUserModel;
+begin
+  Self.selectedUserId := StrToInt(Self.gridUsers.Cells[0, Self.gridUsers.Row]);
+
+  user := Self.controller.GetUser(Self.selectedUserId);
+
+  try
+    Self.editName.Text := user.name;
+    Self.editLogin.Text := user.login;
+  finally
+    user.Free;
+  end;
+
+  Self.pcontrolUser.ActivePage := Self.pcontrolUser.Pages[1];
+end;
+
+procedure TformUser.buttonSaveClick(Sender: TObject);
+var
+  data: TUserDTO;
+  user: TUserModel;
+  errors: TStringList;
+begin
+  data.name := editName.Text;
+  data.login := editLogin.Text;
+  data.password := editPassword.Text;
+
+  if Self.selectedUserId <> 0 then begin
+    errors := data.ValidateDTO(False);
+    try
+      if errors.Count > 0 then raise Exception.Create(errors.Text);    
+    finally
+      errors.Free;
+    end;
+    
+    user := Self.controller.EditUser(Self.selectedUserId, data);
+    user.Free;
+
+    Self.selectedUserId := 0;
+  end else begin
+    errors := data.ValidateDTO(True);
+    try
+      if errors.Count > 0 then raise Exception.Create(errors.Text);    
+    finally
+      errors.Free;
+    end;
+
+    user := Self.controller.CreateUser(data);
+    user.Free;
+  end;
+
+  Self.ClearEdits;
+  Self.pcontrolUser.ActivePage := Self.pcontrolUser.Pages[0];
+end;
+
+procedure TformUser.ClearEdits;
+begin
+  Self.editName.Clear;
+  Self.editLogin.Clear;
+  Self.editPassword.Clear;
 end;
 
 constructor TformUser.Create(AOwner: TComponent);
@@ -67,7 +160,11 @@ begin
     Cells[0, 0] := 'Id';
     Cells[1, 0] := 'Name';
     Cells[2, 0] := 'Login';
+    ColWidths[0] := 100;
+    ColWidths[1] := 250;
+    ColWidths[2] := 250;
 
+    if aUsers <> nil then
     for user in aUsers do begin
       i := RowCount;
       RowCount := RowCount + 1;
