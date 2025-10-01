645f7141c14d88fc7f6e65b6fab1d34155f4997d
mthsgimenez <82736724+mthsgimenez@users.noreply.github.com>
2025-10-01 17:15:10 -0300

    :sparkles: feat: Script para popular tabela permissions

---
+++ seeder.sql
@@ -0,0 +1,4 @@
+insert into permissions values 
+	(1, 'users_register', 'Permite cadastrar usuários'),
+	(2, 'users_edit', 'Permite editar dados de usuários existentes'),
+	(3, 'users_permissions', 'Permite alterar as permissões de outros usuários');
