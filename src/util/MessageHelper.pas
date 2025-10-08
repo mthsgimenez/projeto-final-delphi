89b9eef6487ba4688467ab580b2435fb59b6b8be
mthsgimenez <82736724+mthsgimenez@users.noreply.github.com>
2025-10-08 17:11:46 -0300

    :sparkles: feat: Criado helper para mostrar dialogos

---
+++ src/util/MessageHelper.pas
@@ -0,0 +1,66 @@
+unit MessageHelper;
+
+interface
+
+uses Vcl.Dialogs, System.UITypes;
+
+type TMessageHelper = class
+  private
+    constructor Create;
+    class var instance: TMessageHelper;
+  public
+    class function GetInstance: TMessageHelper;
+    procedure Warning(aMessage: String);
+    procedure Error(aMessage: String);
+    function Confirmation(aMessage: String): Boolean;
+end;
+
+implementation
+
+{ TMessageHelper }
+
+function TMessageHelper.Confirmation(aMessage: String): Boolean;
+var
+  res: Integer;
+begin
+  res := MessageDlg(
+    aMessage,
+    mtConfirmation,
+    [mbYes, mbNo],
+    0);
+
+  Result := res = mrYes;
+end;
+
+constructor TMessageHelper.Create;
+begin
+
+end;
+
+procedure TMessageHelper.Error(aMessage: String);
+begin
+  MessageDlg(
+    aMessage,
+    mtError,
+    [mbOk],
+    0);
+end;
+
+class function TMessageHelper.GetInstance: TMessageHelper;
+begin
+  if instance = nil then
+    instance := TMessageHelper.Create;
+
+  Result := instance;
+end;
+
+procedure TMessageHelper.Warning(aMessage: String);
+begin
+  MessageDlg(
+    aMessage,
+    mtWarning,
+    [mbOk],
+    0);
+end;
+
+end.
