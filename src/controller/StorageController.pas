8cfe2523ebf8a30cb549175837fdf515be9c1e21
mthsgimenez <82736724+mthsgimenez@users.noreply.github.com>
2025-11-04 16:45:58 -0300

    :sparkles: feat (StorageController): Adicionado controller de estoque (#25)

--- src/controller/StorageController.pas
+++ src/controller/StorageController.pas
@@ -2,14 +2,19 @@ unit StorageController;
 
 interface
 
-uses StorageModel, StorageDTO, StorageRepository;
+uses StorageRepository, StorageModel, StorageDTO, System.Generics.Collections, System.SysUtils;
 
 type TStorageController = class
   private
     storageRepository: TStorageRepository;
   public
     constructor Create(aStorageRepository: TStorageRepository);
-end;
+    function CreateStorage(aStorage: TStorageDTO): TStorage;
+    function EditStorage(aId: Integer; aData: TStorageDTO): TStorage;
+    function GetStorage(aId: Integer): TStorage;
+    function GetStorages: TObjectList<TStorage>;
+    function DeleteStorage(aId: Integer): Boolean;
+  end;
 
 implementation
 
@@ -17,7 +22,55 @@ implementation
 
 constructor TStorageController.Create(aStorageRepository: TStorageRepository);
 begin
+  inherited Create;
   Self.storageRepository := aStorageRepository;
 end;
 
+function TStorageController.CreateStorage(aStorage: TStorageDTO): TStorage;
+var
+  storage: TStorage;
+begin
+  Result := nil;
+
+  storage := TStorage.Create;
+  try
+    storage.name := aStorage.name;
+
+    Result := Self.storageRepository.Save(storage);
+  finally
+    storage.Free;
+  end;
+end;
+
+function TStorageController.DeleteStorage(aId: Integer): Boolean;
+begin
+  Result := Self.storageRepository.DeleteById(aId);
+end;
+
+function TStorageController.EditStorage(aId: Integer; aData: TStorageDTO): TStorage;
+var
+  storage: TStorage;
+begin
+  storage := Self.storageRepository.FindById(aId);
+
+  try
+    storage.name := aData.name;
+
+    Result := Self.storageRepository.Save(storage);
+  finally
+    storage.Free;
+  end;
+end;
+
+function TStorageController.GetStorage(aId: Integer): TStorage;
+begin
+  Result := Self.storageRepository.FindById(aId);
+end;
+
+function TStorageController.GetStorages: TObjectList<TStorage>;
+begin
+  Result := Self.storageRepository.FindAll;
+end;
+
 end.
+
