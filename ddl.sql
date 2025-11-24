9d96eb81deb4864a7c8f3e12514f10c049a580d3
mthsgimenez <82736724+mthsgimenez@users.noreply.github.com>
2025-11-24 17:15:44 -0300

    :hammer: chore: Atualização DDL e dados para popular o banco

--- ddl.sql
+++ ddl.sql
@@ -122,18 +122,43 @@ CREATE TABLE service_order_tools (
 	CONSTRAINT service_order_tools_tools_fk FOREIGN KEY (id_tool) REFERENCES tools(id) ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
--- DROP TABLE service_order_tools;
--- DROP TABLE purchase_order_tools;
--- DROP TABLE permission_groups_permissions;
--- DROP TABLE tools;
--- DROP TABLE tools_models;
--- DROP TABLE service_orders;
--- DROP TABLE purchase_orders;
--- DROP TABLE suppliers;
--- DROP TABLE storages;
--- DROP TABLE permissions;
--- DROP TABLE users;
--- DROP TABLE permission_groups;
+INSERT INTO permissions VALUES
+	(1, 'users_create', 'Permite cadastrar usuários'),
+	(2, 'users_update', 'Permite editar dados de usuários existentes'),
+	(3, 'users_delete', 'Permite desativar usuários'),
+	(4, 'group_permissions', 'Permite criar e alterar grupos de permissões'),
+	(5, 'suppliers_create', 'Permite cadastrar fornecedores'),
+	(6, 'suppliers_update', 'Permite alterar dados de fornecedores'),
+	(7, 'suppliers_delete', 'Permite desativar fornecedores'),
+	(8, 'tools_create', 'Permite cadastrar ferramentas'),
+	(9, 'tools_update', 'Permite alterar dados de ferramentas'),
+	(10, 'tools_delete', 'Permite desativar ferramentas'),
+	(11, 'storage', 'Permite visualizar e movimentar o estoque'),
+	(12, 'orders', 'Permite emitir e cancelar ordens de serviço e de compra'),
+	(13, 'receipt', 'Permite receber ordens de compra e de serviço, dando entrada no estoque'),
+	(14, 'reports', 'Permite emitir relatórios');
+
+INSERT INTO permission_groups ("name") VALUES ('admin');
+
+INSERT INTO permission_groups_permissions (id_pgroup, id_permission) VALUES
+	(1, 1),
+	(1, 2),
+	(1, 3),
+	(1, 4),
+	(1, 5),
+	(1, 6),
+	(1, 7),
+	(1, 8),
+	(1, 9),
+	(1, 10),
+	(1, 11),
+	(1, 12),
+	(1, 13),
+	(1, 14);
+
+-- Cria um usuário 'admin' com a senha 'admin'
+INSERT INTO users ("name", login, hash, id_pgroup) VALUES
+('admin', 'admin', '$2a$12$0G6QqmVBdx3i5KvcRQYauOk265bdBTC2DPA7a6RaUm1hLMU3fJ5ey', 1)
 
 -- ALTER SEQUENCE storages_id_seq RESTART WITH 1;
 -- ALTER SEQUENCE suppliers_id_seq RESTART WITH 1;
@@ -146,3 +171,16 @@ CREATE TABLE service_order_tools (
 -- ALTER SEQUENCE purchase_order_tools_id_seq RESTART WITH 1;
 -- ALTER SEQUENCE tools_id_seq RESTART WITH 1;
 -- ALTER SEQUENCE service_order_tools_id_seq RESTART WITH 1;
+
+-- DROP TABLE service_order_tools;
+-- DROP TABLE purchase_order_tools;
+-- DROP TABLE permission_groups_permissions;
+-- DROP TABLE tools;
+-- DROP TABLE tools_models;
+-- DROP TABLE service_orders;
+-- DROP TABLE purchase_orders;
+-- DROP TABLE suppliers;
+-- DROP TABLE storages;
+-- DROP TABLE permissions;
+-- DROP TABLE users;
+-- DROP TABLE permission_groups;
